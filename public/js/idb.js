a87dadff9c1ca394f580b3cb398f8d74aca69e58
JeffreyBanh <banh.jeff@gmail.com>
2022-01-25 23:44:44 -0800

    added onto idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,17 +1,19 @@
+const { application } = require("express");
+
 let db;
 
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open('transaction', 1);
 
 request.onupgradeneeded = function(event){
     const db = event.target.result;
-    db.createObjectStore('new_budget', {autoIncrement: true});
+    db.createObjectStore('new_transaction', {autoIncrement: true});
 };
 
 request.onsuccess = function(event) {
     db = event.target.result;
 
     if (navigator.onLine){
-        uploadBudget();
+        uploadTransaction();
     }
 };
 
@@ -20,9 +22,49 @@ request.onerror = function(event){
 };
 
 function saveRecord(record){
-    const budget = db.budget(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    const budgetObjectStore = budget.objectStore('new_budget');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
 
-    budgetObjectStore.add(record);
+    transactionObjectStore.add(record);
 }
+
+function uploadTransaction(){
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function(){
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers:{
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type' : 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message){
+                    throw new Error(serverResponse);
+                }
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+
+                transactionObjectStore.clear()
+
+                alert('All saved transaction has been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
