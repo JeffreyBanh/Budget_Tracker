92082e8e0e157fda75995afad1e6bc38f506c6c0
JeffreyBanh <banh.jeff@gmail.com>
2022-01-26 00:12:41 -0800

    manifest.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,16 +2,15 @@ const CACHE_NAME = 'my-site-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/favicon.ico',
-    '/manifest.json',
-    '/app.js',
-    '/assets/css/style.css',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png'
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/js/db.js",
+    "/assets/images/icons/icon-192x192.png",
+    "/assets/images/icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 // Install the service worker
